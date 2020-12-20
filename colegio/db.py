ee13f7b816a0a14dccfcd42cad24159a25d627e4
AdolfoCunquero <adolfocunquero.077@gmail.com>
2020-12-19 23:16:22 -0600

    Mejoras nivel codigo

---
+++ colegio/db.py
@@ -0,0 +1,10 @@
+POSTGRESQL = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db_colegio',
+        'USER': 'postgres',
+        'PASSWORD': 'ADOLFO',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+} 
